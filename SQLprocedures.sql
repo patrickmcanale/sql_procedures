4577e5d3032216a7119d725df36b2e4a9c3b1d23
Pat Canale <73977982+patrickmcanale@users.noreply.github.com>
2021-10-18 13:20:05 -0500

    Add files via upload
    initial commit

---
+++ SQLprocedures.sql
@@ -0,0 +1,61 @@
+-- Create the stored procedure
+CREATE PROCEDURE dbo.cuspSumRideHrsSingleDay
+    -- Declare the input parameter
+	@DateParm date,
+    -- Declare the output parameter
+	@RideHrsOut numeric OUTPUT
+AS
+-- send the row count 
+SET NOCOUNT ON
+BEGIN
+-- Assign query result to @RideHrsOut
+SELECT
+	@RideHrsOut = SUM(DATEDIFF(second, StartDate, EndDate))/3600
+FROM CapitalBikeShare
+-- Cast StartDate as date and compare with @DateParm
+WHERE CAST(StartDate AS date) = @DateParm
+RETURN
+END
+
+
+
+-- Create @RideHrs
+DECLARE @RideHrs AS numeric(18,0)
+-- Execute the stored procedure
+EXEC dbo.cuspSumRideHrsSingleDay
+    -- Pass the input parameter
+	@DateParm = '3/1/2018',
+    -- Store the output in @RideHrs
+	@RideHrsOut = @RideHrs OUTPUT
+-- Select @RideHrs
+SELECT @RideHrs AS RideHours
+
+
+
+-- Alter the stored procedure
+CREATE OR ALTER PROCEDURE dbo.cuspRideSummaryDelete
+	-- Specify @DateParm
+	@DateParm nvarchar(30),
+    -- Specify @Error
+	@Error nvarchar(max) = NULL OUTPUT
+AS
+SET NOCOUNT ON
+BEGIN
+  -- Start of the TRY block
+  BEGIN TRY
+  	  -- Delete
+      DELETE FROM RideSummary
+      WHERE Date = @DateParm
+  -- End of the TRY block
+  END TRY
+  -- Start of the CATCH block
+  BEGIN CATCH
+		SET @Error = 
+		'Error_Number: '+ CAST(ERROR_NUMBER() AS VARCHAR) +
+		'Error_Severity: '+ CAST(ERROR_SEVERITY() AS VARCHAR) +
+		'Error_State: ' + CAST(ERROR_STATE() AS VARCHAR) + 
+		'Error_Message: ' + ERROR_MESSAGE() + 
+		'Error_Line: ' + CAST(ERROR_LINE() AS VARCHAR)
+  -- End of the CATCH block
+  END CATCH
+END;
